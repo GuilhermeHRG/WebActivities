85d6add5bf991bbfd057692428fdae69e320cb10
Guilherme Guelere <guilhermeg.dev@gmail.com>
2023-10-03 20:17:39 -0300

    Update codes

--- form-bd/script.js
+++ form-bd/script.js
@@ -1,26 +1,58 @@
 // URL da API
 const url = 'http://reserva.laboratorio.app.br:10100';
 
-// Dados do produto
-const produto = {
-    nomeProduto: 'Nome do Produto',
-    codigoBarra: 'Código de Barra',
-    descricaoProduto: 'Descrição do Produto',
-    idProduto: 'ID do Produto',
-    valorProduto: 'Valor do Produto'
-};
-
 // Opções da requisição
 const options = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(produto)
+    }
 };
 
-// Faz a requisição para a API
-fetch(url, options)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Erro:', error));
+// Função assíncrona para fazer a requisição para a API
+async function fetchData(url, options) {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log('Erro:', error);
+    }
+}
+
+// Função para adicionar uma linha na tabela
+function adicionarLinhaNaTabela(produto) {
+    const tabela = document.getElementById("tabelaProdutos");
+    const linha = tabela.insertRow(-1);
+    linha.insertCell(0).innerHTML = produto.nome;
+    linha.insertCell(1).innerHTML = produto.codbarra;
+    linha.insertCell(2).innerHTML = produto.desc;
+    linha.insertCell(3).innerHTML = produto._idProduto;
+    linha.insertCell(4).innerHTML = produto.valor;
+}
+
+// Função para pegar dados dos input
+async function adicionarProduto() {
+    const produto = {
+        _idProduto: document.getElementById("idProduto").value,
+        nome: document.getElementById("nomeProduto").value,
+        codbarra: document.getElementById("codigoBarra").value,
+        desc: document.getElementById("descricaoProduto").value,
+        valor: document.getElementById("valorProduto").value,
+    };
+
+    // Imprime o objeto no console
+    console.log(produto);
+
+    // Atualiza as opções da requisição com o corpo sendo o produto
+    options.body = JSON.stringify(produto);
+
+    // Chama a função fetchData e espera pela resposta
+    const data = await fetchData(url, options);
+
+    // Se a resposta for bem-sucedida, adiciona uma linha na tabela
+    if (data) {
+        adicionarLinhaNaTabela(produto);
+    }
+}
